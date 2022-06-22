922f9a38f7c201cff6effacfe1cf24b7119a775e
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-06-22 15:51:58 -0300

    fix: included backend.tf

--- backend.tf
+++ backend.tf
@@ -7,12 +7,12 @@
 #   }
 # }
 
-# terraform {
-#   backend "remote" {
-#     hostname = "app.terraform.io"
-#     organization = "ccoe-ttech"
-#     workspaces {
-#       name = "atlantis-aws"
-#     }
-#   }
-# }
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ccoe-ttech"
+    workspaces {
+      name = "atlantis-aws"
+    }
+  }
+}
